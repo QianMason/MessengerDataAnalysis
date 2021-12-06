4e777fae5f9dc4158eebf48b1a2035d6d6238432
Mason Qian <Thunderpurtz@Masons-MacBook-Pro-2.local>
2021-12-06 15:16:59 -0800

    moving to notebook

--- analyze.py
+++ analyze.py
@@ -1,14 +1,31 @@
-import pandas as pd 
+import pandas as pd
 import os
 import json
 from datetime import datetime
-import tqdm
+#import tqdm
+import csv
 
 directory = "/home/thunderpurtz/Desktop/MessengerAnalysis/JessMessengerAnalysis/jessicagiang_ejka7cjioq/"
+directoryMac = "/Users/Thunderpurtz/Desktop/CSPROJECTS/MessengerDataAnalysis/jessicagiang_ejka7cjioq/"
 
-for filename in os.listdir(directory):
-    if filename.endswith(".json"):
-        f = open(directory + filename)
-        data = json.load(f)
-        for message in data["messages"]:
-            print(message)
+# for filename in os.listdir(directoryMac):
+#     if filename.endswith(".json"):
+#         f = open(directoryMac + filename)
+#         data = json.load(f)
+#         for message in data["messages"]:
+#             try:
+#                 date = datetime.fromtimestamp(message["timestamp_ms"] / 1000).strftime("%Y-%m-%d %H:%M:%S")
+#                 sender = message["sender_name"]
+#                 content = message["content"]
+#                 with open("output.csv", 'a') as csv_file:
+#                     writer = csv.writer(csv_file)
+#                     writer.writerow([date,sender,content])
+
+#             except KeyError:
+#                 pass
+
+# df = pd.read_csv('output.csv')
+# print(df.head())
+
+
+#hist = df.hist(column=[''])
